33e903d5d21da03a6976e6ec9f2f92eacf0f1b3f
Dazedtiara6667 <53506835+Dazedtiara6667@users.noreply.github.com>
2020-01-01 18:07:16 +0530

    Update ProjectEuler1.py

--- Task13/ProjectEuler1.py
+++ Task13/ProjectEuler1.py
@@ -1,8 +1,9 @@
 t = int(input())
 for i in range(t):
-    n = int(input())
+    n =int(input())
     x=(n-1)//3
     y=(n-1)//5
     z=(n-1)//15
-    a=(x/2*(6+(x-1)*3))+(y/2*(10+(y-1)*5))-(z/2*(30+(z-1)*15))
-    print(int(a))
+    a=(3*((x*(x+1)))//2)+(5*((y*(y+1)))//2)-(15*((z*(z+1)))//2)
+    print(a)
+
