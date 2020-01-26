0c91982ed100589c2bb4d5323ffb8f2c66aef89d
Rishab Mudliar <53506835+Dazedtiara6667@users.noreply.github.com>
2020-01-26 18:19:41 +0530

    Delete ProjectEuler1.py

--- Task13/ProjectEuler1.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-for i in range(t):
-    n =int(input())
-    x=(n-1)//3
-    y=(n-1)//5
-    z=(n-1)//15
-    a=(3*((x*(x+1)))//2)+(5*((y*(y+1)))//2)-(15*((z*(z+1)))//2)
-    print(a)
-
